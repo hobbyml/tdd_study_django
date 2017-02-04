94667db909b3a87654d9327ecf299dd30f8ddc5d
Go Okuda <nrjgook@gmail.com>
2017-02-04 15:47:04 +0900

    commit before refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.core.urlresolvers import resolve
@@ -14,6 +15,20 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(),expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(response.content.decode('UTF-8'),expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method= 'POST'
+        request.POST['item_text']= 'A new list item'
+
+        response = home_page(request)
+
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode('utf-8'),expected_html)
+
 
